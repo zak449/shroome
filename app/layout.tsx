bc860bf40a87781a82ed4a34f3c7477e32b8b0f6
zak449 <zak@communityattire.com>
2026-03-16 19:12:02 -0700

    fix: remove duplicate hardcoded icon links from head

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,9 +33,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
